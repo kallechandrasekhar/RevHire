616c1c011a06da06d258b024220242ad32bbb1dc
chandra <chandrasekharkalle468@gmail.com>
2026-03-08 19:06:10 +0530

    first commit

--- Rev-Hire/src/main/java/com/revhire/config/CorsConfig.java
+++ Rev-Hire/src/main/java/com/revhire/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://13.48.70.189")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
